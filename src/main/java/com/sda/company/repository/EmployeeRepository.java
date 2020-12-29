a7e1806e4b48d9dcdc0a2d3e0efaacd3f1380594
Cuza Sergiu <cuza.sergiu@yahoo.com>
2020-12-29 23:45:04 +0200

    14 - commit
    added todo's
    nu reusesc sa asignez proiecte si company la un employee

--- src/main/java/com/sda/company/repository/EmployeeRepository.java
+++ src/main/java/com/sda/company/repository/EmployeeRepository.java
@@ -5,6 +5,8 @@ import com.sda.company.models.Employee;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 // annotarea @Repository nu este obligatorie, dar este bine de mentionat
 @Repository
 public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Integer> {
